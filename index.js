8a811ea7783b20cd56f500a99cd823bb701e5a09
Tammy Metz <tammy@tammymetz.com>
2019-08-09 13:04:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.getElementById('nested').querySelector('.target');
 }
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < lis.length; i++) {
+    var rank = n + parseInt(lis[i].innerHTML, 10);
+    lis[i].innerHTML = rank.toString();
+  }
+}
