076abdf461a926aa3cf16c4d8fb61676cf4af7d1
Tammy Metz <tammy@tammymetz.com>
2019-08-09 13:09:24 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.getElementById('nested').querySelector('.target');
 }
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < lis.length; i++) {
+    var rank = n + parseInt(lis[i].innerHTML, 10);
+    lis[i].innerHTML = rank.toString();
+  }
+}
+
+function deepestChild() {
+  var divs = document.getElementById('grand-node').querySelectorAll('div');
+  return divs[divs.length - 1];
+}
